ebb0932b256d44415b6784ae3f79ac783894135e
Echodarker <1023169401@qq.com>
2021-03-02 19:57:28 +0800

    firstDay

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+//单行注释
+//第一行的第三个单词必须和所在的文件名一样，大小写也一样
+//public class 后面代表定义一个类的名称，类是java当中所有源代码的基本组织单位。
+public class HelloWorld{
+//第二行代表万年不变的写法，代表main方法，这行代表程序的起点
+	public static void main(String[]args){
+		//第三行代表打印输出语句（其实就是屏幕显示），希望显示什么就在小括号中写什么
+		System.out.println("hello world!!!");
+	}
+}
+/*无论这里占用多少行
+.......
+
+都算多行注释
+*/
+	
